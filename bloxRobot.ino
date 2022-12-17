ab017523d6171a08a87688f2686794fd94acf495
Mutaz Lafi <mutazhp@hotmail.com>
2022-12-17 18:16:24 +0300

    .

--- bloxRobot.ino
+++ bloxRobot.ino
@@ -11,7 +11,7 @@
 #include <SD.h>
 
 
-const int ServoPin = 3;
+const int ServoPin = 2;
 
 const int Servo_Right_Angle = 0;
 const int Servo_Center_Angle = 90;
@@ -34,19 +34,21 @@ const int ChipSelect = 53;
 
 int GPS_State = 0;
 int GPS_Count = 0;
+boolean SD_State = false;
 int MaxDistance = 350;
 
-// undefined or comment it to hide Serial Data
-#define DEBUG
-
+#define DEBUG    // undefined or comment it to hide Serial Data
 
 SoftwareSerial Bluetooth (BluetoothRXPin, BluetoothTXPin);
+
 SoftwareSerial GPS_Connection (GPS_RXPin, GPS_TXPin);
+
 NewPing sonar (Trig_Pin, Echo_Pin, MaxDistance);
+
 Servo servo;
+
 TinyGPSPlus GPS;
 
-File DataFile;
 
 #include "classes.h"
 
@@ -66,29 +68,31 @@ void setup() {    // Setup
   Serial.begin(9600);
 #endif
 
+  pinMode(Buzzer, OUTPUT);
 
   if (!SD.begin(ChipSelect)) {
     BuzzerSound();
-    Serial.println("Error opening SD card");
+    Serial.println("Error opening SD card.");
     delay(2000);
-    while (true);
+    ErrorSound();
+
+
   } else {
-    Serial.println("SD Card opened ");
+    Serial.println("SD Card opened. ");
   }
 
+  DataFile = SD.open("ROBOT.TXT", FILE_WRITE);
 
-
-
-
-  DataFile = SD.open("data.txt");
 
   if (DataFile) {
-    Serial.println("file opened");
+    Serial.println("file opened.");
   } else {
-    Serial.println("Error opening the File");
+    Serial.println("Error opening the File.");
+    ErrorSound();
     while (true);
   }
-  DataFile.println("Robot Blox By Mutaz Lafi Data File ");
+  BuzzerSound();
+  DataFile.println("Robot Blox By Mutaz Lafi Data File. ");
 
   Serial.println("Robot Starting....");
 
@@ -97,27 +101,31 @@ void setup() {    // Setup
   BuzzerSound();
   BluetoothMode.Begin(9600);
   servo.attach(ServoPin);
-  Sounds.Begin();
+  pinMode(Buzzer, OUTPUT);
   GPS_Connection.begin(9600);
 
 
 
-  Serial.println("Bluetooth,Servo and Sound Started");
-  DataFile.println("Bluetooth,Servo and Sound Started");
+  Serial.println("Bluetooth,Servo and Sound Started.");
+  DataFile.println("Bluetooth,Servo and Sound Started.");
 
   servo.write(Servo_Center_Angle);
 
-  DataFile.println("Servo is on 90 Angle");
-  Serial.println("Servo is on 90 Angle");
+  DataFile.println("Servo is on 90 Angle.");
+  Serial.println("Servo is on 90 Angle.");
   delay(500);
   Serial.println("Getting Some infromations from the Ultrasonic sensor....");
   DataFile.println("Getting Some infromations from the Ultrasonic sensor....");
+
   Explorer.Begin();
   Serial.println("Done.");
   DataFile.println("Done.");
 
- 
-GPSCheck:     // goto label
+  Serial.println("AL Sensors And Modules Started in The Robot.");
+  DataFile.println("AL Sensors And Modules Started in The Robot.");
+
+GPSCheck:             // goto label
+
   GPS_Logging.GPSLog();
 
   if (GPS_State == 0) {
@@ -125,11 +133,12 @@ GPSCheck:     // goto label
     Serial.println(GPS_State);
     GPS_Count++;
     delay(100);
-    if (GPS_Count == 3000) {
+    Serial.print(GPS_Count);
+    if (GPS_Count >= 500) {
       Serial.print("GPS DATA WAS UNABLE TO BE DETECTED");
       DataFile.println("GPS DATA WAS UNABLE TO BE DETECTED");
       DataFile.close();
-
+      while (true);
     }
     goto GPSCheck;
   }
@@ -143,17 +152,24 @@ GPSCheck:     // goto label
 
 
 void loop() {         //Loop
-
+  if (SD_State == false) {
+    File DataFile = SD.open("ROBOT.TXT");
+    SD_State = true;
+  }
   int ModePinData = map(analogRead(ModePin), 1023, 0, 1000, 0);
 
+  DataFile.print("The Mode Pin is On: ");
+  DataFile.println(ModePinData);
+  DataFile.print("In Mode: ");
   if (ModePinData < 500) {
-    BuzzerSound();
+    DataFile.println("Explorer Mode");
     Explorer.StartExplorer();
   } else {
-    BuzzerSound();
+    DataFile.println("Bluetooth And Sensors Mode");
     Sounds.StartSoundSensor();
     BluetoothMode.StartBluetoothMode();
   }
+  DataFile.close();
 }
 
 void BuzzerSound() {
@@ -161,4 +177,17 @@ void BuzzerSound() {
   delay(2000);
   digitalWrite(Buzzer, LOW);
   delay(1000);
+}
+
+void ErrorSound() {
+  digitalWrite(Buzzer, HIGH);
+  delay(2000);
+  digitalWrite(Buzzer, LOW);
+  delay(500);
+  digitalWrite(Buzzer, HIGH);
+  delay(500);
+  digitalWrite(Buzzer, LOW);
+  delay(500);
+  digitalWrite(Buzzer, HIGH);
+  delay(5000);
 }
